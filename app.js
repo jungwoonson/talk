75fdc467b4664ca0b33cbbeaa42550350ebfa4af
jungwoon727 <jungwoon727@gmail.com>
2022-07-10 09:09:49 +0900

    feat : Add user

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import { config } from './config.js';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import 'express-async-errors';
+import userRouter from './router/user.js';
+import { sequelize } from './db/database.js';
 
 const app = express();
 
@@ -11,4 +14,17 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.listen(config.host.port);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
+
+sequelize.sync().then(() => {
+  const server = app.listen(config.host.port);
+});
---
+++ data/user.js
@@ -0,0 +1,36 @@
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+
+export const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(128),
+    allowNull: true,
+  },
+  name: {
+    type: DataTypes.STRING(128),
+    allowNull: false,
+  },
+});
+
+export async function findByUsername(username) {
+  return User.findOne({ where: { username } });
+}
+
+export async function findById(id) {
+  return User.findByPk(id);
+}
+
+export async function createUser(user) {
+  return User.create(user).then((data) => data.dataValues.id);
+}
